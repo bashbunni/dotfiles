0bc1222c5821586d427382ffb6e97c336875840e
bashbunni <bunni@bashbunni.dev>
2023-03-23 13:32:43 -0700

    mostly working neovim theme

--- .config/nvim/colors/bunnitheme.lua
+++ .config/nvim/colors/bunnitheme.lua
@@ -1,3 +1,5 @@
+-- :h lsp-highlight
+
 require('colorbuddy').setup()
 local Color = require('colorbuddy').Color
 local Group = require('colorbuddy').Group
@@ -19,7 +21,8 @@ v.colors_name = 'bunnitheme'
 local palette = {
     background = "#171717",
     foreground = "#C5C8C6",
-    comment = "#282a2e",
+    comment = "#504F4F",
+    cyan = "#04D7D7",
     green = "#31BB71",
     pink = "#FF64C8",
     purple = "#6B50FF",
@@ -32,24 +35,41 @@ for key, value in pairs(palette) do
     Color.new(key, value)
 end
 
+
 Group.new('Normal', c.foreground, c.none, no)
 Group.new('String', c.green, c.none, no)
 Group.new('Comment', c.comment, c.none, i)
-Group.new('Underlined', c.green, c.none, ul)
+Group.new('SpecialComment', c.foreground, c.purple, i)
+Group.new("Warnings", c.yellow, c.none, no)
 Group.new('Error', c.red, c.none, no)
 Group.new('Todo', c.cyan, c.none, no)
 Group.new('Special', c.foreground, c.none, no)
 Group.new('Identifier', c.foreground, c.none, no)
-Group.new('Label', c.foreground, c.none, no)
-Group.new('LineNr', c.foreground, c.none, no)
+Group.new('Label', c.purple, c.none, no)
+Group.new('LineNr', c.comment, c.none, no)
 Group.new('Typedef', c.purple, c.none, no)
 Group.new('Conditional', c.pink, c.none, no)
-Group.new('Function', c.purple, c.none, no)
+Group.new('Function', c.cyan, c.none, no)
+Group.new('Statement', c.red, c.none, no)
 Group.new('Include', c.yellow, c.none, no)
-Group.new('Operator', c.foreground, c.none, no)
+Group.new('Operator', c.purple, c.none, no)
+Group.new('MatchParen', c.background, c.pink, no)
+Group.new('Constant', c.yellow, c.none, b)
+Group.new('Boolean', c.purple, c.none, no)
+Group.new('Number', c.pink, c.none, no)
+Group.new('Structure', c.purple, c.none, no)
+Group.new('Cursor', c.foreground, c.purple, no)
 
-Group.new('StatusLine', c.pink, c.none, no)
-Group.new('StatusLineTermNC', c.pink, c.none, no)
+-- LSP: this one seems to do the trick on nightly
+Group.new('DiagnosticError', c.red, c.none, i)
+Group.new('DiagnosticWarn', c.yellow, c.none, i)
+Group.new('DiagnosticHint', c.green, c.none, i)
+Group.new('DiagnosticInfo', c.yellow, c.none, i)
+
+-- diff
+Group.new('diffAdded', c.green, c.none, no)
+Group.new('diffRemoved', c.red, c.none, no)
+Group.new('diffChanged', c.pink, c.none, no)
 
 -- status line of current window
--- yes ok
+Group.new('StatusLine', c.none, c.comment, no)
