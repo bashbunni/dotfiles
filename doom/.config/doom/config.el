19dccafbd01bd18c240d76e0f3de50876fd0c7a3
bashbunni <bunni@bashbunni.dev>
2024-09-06 11:15:20 -0700

    feat(theme): use catppuccin theme

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -32,7 +32,10 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'catppuccin)
+(setq catppuccin-flavor 'macchiato) ; or 'frappe 'latte, 'macchiato, or 'mocha
+    (load-theme 'catppuccin t)
+
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
--- doom/.config/doom/packages.el
+++ doom/.config/doom/packages.el
@@ -55,3 +55,6 @@
   (add-hook 'before-save-hook #'lsp-format-buffer t t)
   (add-hook 'before-save-hook #'lsp-organize-imports t t))
 (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
+;; Theme
+(package! catppuccin-theme)
